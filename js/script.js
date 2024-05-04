9c6fba67fae86ebcba096d02f9e70dfc467f4058
hotyute <hotyute_mason@hotmail.com>
2024-05-05 00:50:49 +0100

    modified:   header.php 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,2 @@
 
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburger = document.querySelector('.hamburger');
-    const navUL = document.querySelector('nav ul');
 
-    hamburger.addEventListener('click', function () {
-        navUL.style.display = navUL.style.display === 'block' ? 'none' : 'block';
-    });
-});
