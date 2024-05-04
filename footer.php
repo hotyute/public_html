b5aea378e13dc033922aa71da571291433abc5fa
hotyute <hotyute_mason@hotmail.com>
2024-05-05 00:36:34 +0100

    modified:   footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 
 <footer style="background-image: url('<?php echo BASE_URL; ?>images/banner.jpg'); background-repeat: no-repeat; background-size: cover; text-align: center; background-position: center -400px;">
-<?php if (!isset($_SESSION['user_id'])): ?>
+<?php if (!isset($_SESSION['username'])): ?>
     <p>Not registered yet? <a href="register.php">Register here</a></p>
 <?php endif; ?>
 <p>&copy; <?php echo date("Y"); ?> Christian Community. All rights reserved.</p>
