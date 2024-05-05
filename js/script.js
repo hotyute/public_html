ef34e410a485385d1f9bfdf15b5fe1b265582421
hotyute <hotyute_mason@hotmail.com>
2024-05-05 11:51:50 +0100

    modified:   js/script.js 	modified:   style.css

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const navUL = document.querySelector('nav ul');
 
     hamburger.addEventListener('click', function () {
-        navUL.style.display = navUL.style.display === 'block' ? 'none' : 'block';
+        // Toggle class for sliding animation
+        navUL.classList.toggle('open');
     });
 });
