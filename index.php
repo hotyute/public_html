902536a27b2a2613ba22dedc368faf25e20cd8f4
hotyute <hotyute_mason@hotmail.com>
2024-05-10 23:19:44 +0100

    modified:   index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
                     }
                     echo '<h3>' . htmlspecialchars($post['title']) . '</h3>';
                     echo '<p>By ' . htmlspecialchars($post['author']) . '</p>';
-                    echo '<div class="content-full">' . substr(htmlspecialchars($post['content']), 0, 100) . '...</div>';  // Use content-full class
+                    echo '<div class="content-full">' . substr(htmlspecialchars($post['content']), 0, 75) . '...</div>';  // Use content-full class
                     echo '<div class="content-short">' . substr(htmlspecialchars($post['content']), 0, 50) . '...</div>';  // Use content-short class
                     echo '</a>';
                     echo '</div>';
