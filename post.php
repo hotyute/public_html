3be14deff930fa7e9d0f904460c70be5c15bf63f
hotyute <hotyute_mason@hotmail.com>
2024-05-10 22:32:38 +0100

    deleted:    images/Designer.png 	new file:   images/thumbnail.png 	modified:   post.php 	modified:   styles/style.css

--- post.php
+++ post.php
@@ -3,15 +3,19 @@ include 'header.php';
 require 'includes/database.php';
 
 $post_id = isset($_GET['id']) ? (int) $_GET['id'] : 0;
+
 if ($post_id > 0) {
-    $stmt = $pdo->prepare("SELECT posts.title, posts.content, posts.thumbnail, users.displayname AS author FROM posts JOIN users ON posts.user_id = users.id WHERE posts.id = ?");
+    // Increment the view counter
+    $pdo->exec("UPDATE posts SET views = views + 1 WHERE id = $post_id");
+
+    $stmt = $pdo->prepare("SELECT posts.title, posts.content, posts.thumbnail, users.displayname AS author, posts.views FROM posts JOIN users ON posts.user_id = users.id WHERE posts.id = ?");
     $stmt->execute([$post_id]);
     $post = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($post) {
         echo '<div class="post-container">';
         echo '<h1 class="post-title">' . htmlspecialchars($post['title']) . '</h1>';
-        echo '<h4 class="post-author">By ' . htmlspecialchars($post['author']) . '</h4>';
+        echo '<h4 class="post-author">By ' . htmlspecialchars($post['author']) . ' | Views: ' . $post['views'] . '</h4>';
         if ($post['thumbnail']) {
             echo '<img src="' . $post['thumbnail'] . '" alt="Post Image" class="post-thumbnail">';
         }
@@ -24,4 +28,3 @@ if ($post_id > 0) {
     echo '<p class="post-error">Invalid post ID.</p>';
 }
 include 'footer.php';
-?>
