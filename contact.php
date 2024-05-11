4097d79a24acc4e6b9a0d29264f10a423ab67f0e
hotyute <hotyute_mason@hotmail.com>
2024-05-11 20:36:04 +0100

    modified:   contact.php

--- contact.php
+++ contact.php
@@ -1,6 +1,8 @@
 <?php include 'header.php'; ?>
 <div class="main-container">
-    <h2>Contact Us</h2>
-    <p>This is the contact page. Feel free to reach out with any questions or comments.</p>
+    <main>
+        <h2>Contact Us</h2>
+        <p>This is the contact page. Feel free to reach out with any questions or comments.</p>
+    </main>
 </div>
 <?php include 'footer.php'; ?>
