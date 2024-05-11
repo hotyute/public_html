2fdf9b9be2077c34b280a4fc8bd28140fab1dd51
hotyute <hotyute_mason@hotmail.com>
2024-05-11 19:05:58 +0100

    new file:   submit_comment.php

---
+++ submit_comment.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require 'includes/database.php';
+
+header('Content-Type: application/json');
+
+if (isset($_POST['comment'], $_POST['user_id'], $_POST['post_id'])) {
+    // Assume $pdo is your database connection from a PDO instance
+    $stmt = $pdo->prepare("INSERT INTO comments (post_id, user_id, content) VALUES (?, ?, ?)");
+    if ($stmt->execute([$_POST['post_id'], $_POST['user_id'], htmlspecialchars($_POST['comment'])])) {
+        echo json_encode(['success' => true]);
+    } else {
+        echo json_encode(['success' => false]);
+    }
+} else {
+    echo json_encode(['success' => false, 'message' => 'Incomplete data']);
+}
+?>
