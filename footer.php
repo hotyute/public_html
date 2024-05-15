19f8da48b7db56324fc5a3f7a2fc918b1f8f680e
hotyute <hotyute_mason@hotmail.com>
2024-05-15 11:27:17 +0100

    modified:   footer.php 	modified:   index.php 	new file:   js/post-preview.js

--- footer.php
+++ footer.php
@@ -1,9 +1,13 @@
-
 <footer style="background-image: url('<?php echo BASE_URL; ?>images/banner.jpg'); background-repeat: no-repeat; background-size: cover; text-align: center; background-position: center -400px;">
-<?php if (!isset($_SESSION['username'])): ?>
-    <p>Not registered yet? <a href="register.php">Register here</a></p>
-<?php endif; ?>
-<p>&copy; <?php echo date("Y"); ?> Christian Community. All rights reserved.</p>
+    <?php if (!isset($_SESSION['username'])) : ?>
+        <p>Not registered yet? <a href="register.php">Register here</a></p>
+    <?php endif; ?>
+    <p>&copy; <?php echo date("Y"); ?> Christian Community. All rights reserved.</p>
 </footer>
 </body>
+
 </html>
+
+<?php if ($current_page == 'index') : ?>
+    <script src="<?php echo BASE_URL; ?>js/post-preview.js"></script>
+<?php endif; ?>
