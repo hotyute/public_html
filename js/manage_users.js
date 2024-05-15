291e2c9073c3e2a84930f2684ac3fadab50e3a90
hotyute <hotyute_mason@hotmail.com>
2024-05-15 23:02:58 +0100

    modified:   js/manage_users.js 	renamed:    includes/users/search_users.php -> js/search_users.php

--- js/manage_users.js
+++ js/manage_users.js
@@ -3,7 +3,7 @@ document.getElementById('searchForm').addEventListener('submit', function(event)
     event.preventDefault();
     let query = document.getElementById('searchQuery').value;
 
-    fetch(`/includes/users/search_users.php?query=${query}`)
+    fetch(`search_users.php?query=${query}`)
         .then(response => response.json())
         .then(data => {
             let resultsDiv = document.getElementById('searchResults');
