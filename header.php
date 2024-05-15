6426de72858eb39d0ff6c569a7d661d9740f022a
hotyute <hotyute_mason@hotmail.com>
2024-05-15 07:57:38 +0100

    modified:   header.php 	new file:   styles/featuredvid.css

--- header.php
+++ header.php
@@ -10,6 +10,9 @@ if (isset($_GET['logout'])) {
     header("Location: " . BASE_URL . "login.php"); // Redirect to the login page after logout
     exit();
 }
+
+// Get the current script name
+$current_page = basename($_SERVER['SCRIPT_NAME'], '.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,10 +21,24 @@ if (isset($_GET['logout'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/style.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/surplus.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/admin.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/post.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/comments.css">
+    
+    <?php if ($current_page == 'index'): ?>
+        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/featuredvid.css">
+    <?php endif; ?>
+    
+    <?php if ($current_page == 'admin_panel'): ?>
+        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/admin.css">
+    <?php endif; ?>
+    
+    <?php if ($current_page == 'post'): ?>
+        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/post.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/comments.css">
+    <?php endif; ?>
+    
+    <?php if ($current_page == 'contact'): ?>
+        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/surplus.css">
+    <?php endif; ?>
+
     <script src="<?php echo BASE_URL; ?>js/script.js"></script>
     <title>MyGospel Christian Website</title>
     <script>
