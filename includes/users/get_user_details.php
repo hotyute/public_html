83a8cbf57122862051deb65d82193344c33db507
hotyute <hotyute_mason@hotmail.com>
2024-05-15 21:08:44 +0100

    modified:   includes/users/get_user_details.php

--- includes/users/get_user_details.php
+++ includes/users/get_user_details.php
@@ -4,10 +4,10 @@ require_once '../../base_config.php';
 include('includes/config.php');
 include('includes/database.php');
 
-$userName = $_GET['id'];
+$userId = $_GET['id'];
 
-$stmt = $pdo->prepare("SELECT username, displayname, role FROM users WHERE username = ?");
-$stmt->execute([$userName]);
+$stmt = $pdo->prepare("SELECT id, displayname, role FROM users WHERE id = ?");
+$stmt->execute([$userId]);
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
 echo json_encode($user);
