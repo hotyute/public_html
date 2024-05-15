7c7879b5b803c40fee1f90b081394f63fc358fe3
hotyute <hotyute_mason@hotmail.com>
2024-05-15 22:38:51 +0100

    modified:   header.php

--- header.php
+++ header.php
@@ -21,30 +21,30 @@ $current_page = basename($_SERVER['SCRIPT_NAME'], '.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/style.css">
-    
-    <?php if ($current_page == 'index'): ?>
+
+    <?php if ($current_page == 'index') : ?>
         <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/surplus.css">
         <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/featuredvid.css">
     <?php endif; ?>
 
-    <?php if ($current_page == 'login'): ?>
-        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/surplus.css">
-    <?php endif; ?>
-    
-    <?php if (($current_page == 'admin_panel') || ($current_page ==  'edit_post')): ?>
-        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/admin.css">
-    <?php endif; ?>
-    
-    <?php if ($current_page == 'post'): ?>
-        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/post.css">
-        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/comments.css">
-    <?php endif; ?>
-    
-    <?php if ($current_page == 'contact'): ?>
+    <?php if ($current_page == 'login') : ?>
         <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/surplus.css">
     <?php endif; ?>
 
-    <?php if ($current_page == 'archive'): ?>
+    <?php if (($current_page == 'admin_panel') || ($current_page ==  'edit_post') || ($current_page == 'create_post')): ?>
+        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/admin.css">
+    <?php endif; ?>
+
+    <?php if ($current_page == 'post') : ?>
+        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/post.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/comments.css">
+    <?php endif; ?>
+
+    <?php if ($current_page == 'contact') : ?>
+        <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/surplus.css">
+    <?php endif; ?>
+
+    <?php if ($current_page == 'archive') : ?>
         <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>styles/archive.css">
     <?php endif; ?>
 
