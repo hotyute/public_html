17d1b219d1c814c961dded16c98ecd98ebfc0bfe
hotyute <hotyute_mason@hotmail.com>
2024-05-16 19:30:41 +0100

    modified:   setup.php

--- setup.php
+++ setup.php
@@ -23,7 +23,7 @@ CREATE TABLE users (
     username VARCHAR(255) UNIQUE NOT NULL,
     displayname VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role ENUM('admin', 'editor', 'member') DEFAULT 'member'
+    rights ENUM('admin', 'editor', 'member') DEFAULT 'member'
 );
 
 CREATE TABLE IF NOT EXISTS posts (
