98e8e3e30b82461188da282b73a4df48a9f695b5
hotyute <hotyute_mason@hotmail.com>
2024-05-17 09:34:14 +0100

    modified:   includes/users/search_users.php

--- includes/users/search_users.php
+++ includes/users/search_users.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 $searchQuery = $_GET['query'];
 
 $stmt = $pdo->prepare("SELECT id, username, displayname, role FROM users WHERE username = ?");
-$stmt->execute([$searchQuery, $searchQuery]);
+$stmt->execute([$searchQuery]);
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 echo json_encode($results);
