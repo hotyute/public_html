83c35e2a365346396168212088f720ba81126a67
hotyute <hotyute_mason@hotmail.com>
2024-05-17 09:23:49 +0100

    modified:   includes/users/search_users.php

--- includes/users/search_users.php
+++ includes/users/search_users.php
@@ -2,9 +2,9 @@
 <?php
 require '../database.php';
 
-$searchQuery = '%' . $_GET['query'] . '%';
+$searchQuery = $_GET['query'];
 
-$stmt = $pdo->prepare("SELECT id, username, displayname, role FROM users WHERE username LIKE ? OR displayname LIKE ?");
+$stmt = $pdo->prepare("SELECT id, username, displayname, role FROM users WHERE username = ?");
 $stmt->execute([$searchQuery, $searchQuery]);
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
