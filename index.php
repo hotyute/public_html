4bf8d7ec67c06b4467d10398779e06fca93e2b21
hotyute <hotyute_mason@hotmail.com>
2024-05-19 18:26:27 +0100

    modified:   index.php

--- index.php
+++ index.php
@@ -21,9 +21,9 @@
                     if ($post['thumbnail']) {
                         echo '<img src="' . $post['thumbnail'] . '" alt="Post thumbnail" class="post-thumbnail">';
                     }
-                    echo '<h3>' . htmlspecialchars($post['title']) . '</h3>';
-                    echo '<p>By ' . htmlspecialchars($post['author']) . '</p>';
-                    echo '<div class="content-preview" data-content="' . htmlspecialchars($post['content']) . '"></div>';
+                    echo '<h3>' . htmlspecialchars_decode($post['title']) . '</h3>';
+                    echo '<p>By ' . htmlspecialchars_decode($post['author']) . '</p>';
+                    echo '<div class="content-preview" data-content="' . htmlspecialchars_decode($post['content']) . '"></div>';
                     echo '<p class="comment-count">' . $post['comment_count'] . ' Comments</p>';
                     echo '</a>';
                     echo '</div>';
