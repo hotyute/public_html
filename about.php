b5e465b2a0efb75d64421949d6f0a579b186a178
hotyute <hotyute_mason@hotmail.com>
2024-05-25 21:49:37 +0100

    modified:   about.php

--- about.php
+++ about.php
@@ -1,3 +1,10 @@
 <?php include 'header.php'; ?>
-
+<div class="main-container">
+    <main>
+        <section>
+            <h2>About Us</h2>
+            <p>This is the about us page. We are a community devoted to spreading and understanding the Word of God.</p>
+        </section>
+    </main>
+</div>
 <?php include 'footer.php'; ?>
