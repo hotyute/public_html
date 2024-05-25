5b95b728ba6d385c8a277f8568819beba050874f
hotyute <hotyute_mason@hotmail.com>
2024-05-25 20:26:26 +0100

    modified:   post.php

--- post.php
+++ post.php
@@ -47,12 +47,13 @@ if ($post_id > 0) {
         echo '<div class="post-content">' . nl2br($content_page) . '</div>';
 
         // Pagination controls
-        echo '<div class="pagination" style="display: flex; justify-content: space-between;">';
+        echo '<div class="pagination" style="display: flex; justify-content: space-between; align-items: center;">';
         if ($page > 1) {
             echo '<a href="post.php?id=' . $post_id . '&page=' . ($page - 1) . '">Previous</a>';
         } else {
             echo '<span></span>'; // Empty span to maintain space
         }
+        echo '<span>Page ' . $page . ' of ' . $total_pages . '</span>';
         if ($page < $total_pages) {
             echo '<a href="post.php?id=' . $post_id . '&page=' . ($page + 1) . '">Next</a>';
         }
