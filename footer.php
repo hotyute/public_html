d5245a6e87094950732d632e383f88cedd1cf3e6
hotyute <hotyute_mason@hotmail.com>
2024-05-27 19:14:21 +0100

    modified:   footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
+<?php if ($current_page == 'roster') : ?>
+    <script src="<?php echo BASE_URL; ?>js/roster.js"></script>
+<?php endif; ?>
+
 <footer style="background-image: url('<?php echo BASE_URL; ?>images/banner.jpg'); background-repeat: no-repeat; background-size: cover; text-align: center; background-position: center -400px;">
     <?php if (!isset($_SESSION['username'])) : ?>
         <p>Not registered yet? <a href="register.php">Register here</a></p>
@@ -13,7 +17,4 @@
 <?php endif; ?>
 <?php if ($current_page == 'manage_users') : ?>
     <script src="<?php echo BASE_URL; ?>js/manage_users.js"></script>
-<?php endif; ?>
-<?php if ($current_page == 'roster') : ?>
-    <script src="<?php echo BASE_URL; ?>js/roster.js"></script>
 <?php endif; ?>
