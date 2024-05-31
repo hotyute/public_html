d041dbe62435175e326254add0e7a80fec0a7b8c
hotyute <hotyute_mason@hotmail.com>
2024-05-31 14:32:57 +0100

    modified:   roster.php

--- roster.php
+++ roster.php
@@ -1,4 +1,4 @@
-<?php include 'header.php'; 
+<?php include 'header.php';
 
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
@@ -6,20 +6,22 @@ if (session_status() == PHP_SESSION_NONE) {
 
 ?>
 
-<form id="searchForm">
-    <input type="text" id="searchQuery" placeholder="Search users...">
-    <button type="submit">Search</button>
-</form>
-<div id="searchResults"></div>
-<table id="rosterTable" border="1">
-    <tr>
-        <th>Username</th>
-        <th>Display Name</th>
-        <th>Role</th>
-        <th>Devotion</th>
-    </tr>
-</table>
+<div class="admin-content">
+    <form id="searchForm">
+        <input type="text" id="searchQuery" placeholder="Search users...">
+        <button type="submit">Search</button>
+    </form>
+    <div id="searchResults"></div>
+    <table id="rosterTable" border="1">
+        <tr>
+            <th>Username</th>
+            <th>Display Name</th>
+            <th>Role</th>
+            <th>Devotion</th>
+        </tr>
+    </table>
 
-<p style="border:2px solid DodgerBlue; color:#FF0000; font-size: 180%; text-align: center;">These members have been displayed for testing purposes. Other members have been hidden due privacy concerns.</p>
+    <p style="border:2px solid DodgerBlue; color:#FF0000; font-size: 180%; text-align: center;">These members have been displayed for testing purposes. Other members have been hidden due privacy concerns.</p>
+</div>
 
 <?php include 'footer.php'; ?>
