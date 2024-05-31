31bcecbe5225fa533b8550c4e1abcd53aaf5e1e9
hotyute <hotyute_mason@hotmail.com>
2024-05-31 13:29:27 +0100

    modified:   search.php 	modified:   header.php

--- search.php
+++ search.php
@@ -2,7 +2,9 @@
 require_once 'includes/database.php';
 require_once 'base_config.php';
 
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
