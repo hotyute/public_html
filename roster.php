c310ee49c10968a3c4d9323e0e91e4900be62c50
hotyute <hotyute_mason@hotmail.com>
2024-05-31 14:33:56 +0100

    modified:   roster.php

--- roster.php
+++ roster.php
@@ -6,7 +6,6 @@ if (session_status() == PHP_SESSION_NONE) {
 
 ?>
 
-<div class="admin-content">
     <form id="searchForm">
         <input type="text" id="searchQuery" placeholder="Search users...">
         <button type="submit">Search</button>
@@ -22,6 +21,5 @@ if (session_status() == PHP_SESSION_NONE) {
     </table>
 
     <p style="border:2px solid DodgerBlue; color:#FF0000; font-size: 180%; text-align: center;">These members have been displayed for testing purposes. Other members have been hidden due privacy concerns.</p>
-</div>
 
 <?php include 'footer.php'; ?>
