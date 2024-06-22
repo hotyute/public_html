3d0b4d9076742daf9ac8d1c566e467a5bb3c5a62
hotyute <hotyute_mason@hotmail.com>
2024-06-22 15:19:51 +0100

    modified:   admin/test_manage.php

--- admin/test_manage.php
+++ admin/test_manage.php
@@ -2,6 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 session_start();
+require_once '../base_config.php';
 require 'includes/config.php';
 
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
