96ba11df5f42e582892853279a22916e57a86992
hotyute <hotyute_mason@hotmail.com>
2024-07-27 15:55:42 +0100

    modified:   contact.php

--- contact.php
+++ contact.php
@@ -4,6 +4,7 @@
         <section>
             <h2>Contact Us</h2>
             <p>This is the contact page. Feel free to reach out with any questions or comments.</p>
+            <p>admin@divineword.co.uk</p>
         </section>
     </main>
 </div>
