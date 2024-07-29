df2d64ff502c15ae01d75d7bb1e1447e403d6c57
hotyute <hotyute_mason@hotmail.com>
2024-07-29 02:28:00 +0100

    modified:   footer.php 	modified:   styles/footer.css

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@ $footerLinks = [
 ];
 ?>
 
-<footer class="footer">
+<footer style="background-image: url('<?php echo BASE_URL; ?>images/banner.jpg'); background-repeat: no-repeat; background-size: cover; text-align: center; background-position: center -400px;">
     <?php if (!isset($_SESSION['username'])) : ?>
         <p>Not registered yet? <a href="register.php">Register here</a></p>
     <?php endif; ?>
@@ -29,26 +29,23 @@ $footerLinks = [
         <ul>
             <?php foreach ($footerLinks as $link) : ?>
                 <li>
-                    <div class="footer-link-item">
-                        <?php if ($link['thumbnail']) : ?>
+                    <?php if ($link['thumbnail']) : ?>
+                        <div class="footer-link-item">
                             <img src="<?php echo $link['thumbnail']; ?>" alt="<?php echo $link['text']; ?>" class="footer-thumbnail">
-                        <?php endif; ?>
-                        <a href="<?php echo $link['url']; ?>"><?php echo $link['text']; ?></a>
-                    </div>
+                            <a href="<?php echo $link['url']; ?>"><?php echo $link['text']; ?></a>
+                        </div>
+                    <?php else : ?>
+                        <div class="footer-link-item">
+                            <a href="#"><?php echo $link['text']; ?></a>
+                        </div>
+                    <?php endif; ?>
                 </li>
             <?php endforeach; ?>
         </ul>
     </div>
-
-    <!-- Social Media Icons (example) -->
-    <div class="social-icons">
-        <a href="#"><i class="fab fa-facebook-f"></i></a>
-        <a href="#"><i class="fab fa-twitter"></i></a>
-        <a href="#"><i class="fab fa-instagram"></i></a>
-        <a href="#"><i class="fab fa-linkedin-in"></i></a>
-    </div>
 </footer>
 </body>
+
 </html>
 
 <?php if ($current_page == 'index') : ?>
