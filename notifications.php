3f15acfe1d7573086ecc4f3026d72536ba51e0de
hotyute <hotyute_mason@hotmail.com>
2024-08-01 23:12:42 +0100

    modified:   notifications.php

--- notifications.php
+++ notifications.php
@@ -1,31 +1,31 @@
 <?php 
-include 'header.php';
-include_once 'includes/notifications/get_notification_data.php';
+// include 'header.php';
+// include_once 'includes/notifications/get_notification_data.php';
 
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
+// if (session_status() == PHP_SESSION_NONE) {
+//     session_start();
+// }
 
-if (!isset($_SESSION['username'])) {
-    header('Location: login.php');
-    exit();
-}
+// if (!isset($_SESSION['username'])) {
+//     header('Location: login.php');
+//     exit();
+// }
 
-$user_id = $_SESSION['user_id'];
-$notifications = get_notifications($user_id, true); // Fetch all notifications
+// $user_id = $_SESSION['user_id'];
+// $notifications = get_notifications($user_id, true); // Fetch all notifications
 
 ?>
 
-<div class="container">
+<!-- <div class="container">
     <h2>Your Notifications</h2>
     <?php
-    if (count($notifications) > 0) {
-        foreach ($notifications as $notification) {
-            echo "<div class='notification'>" . $notification['message'] . " - " . $notification['created_at'] . "</div>";
-        }
-    } else {
-        echo "<p>No notifications.</p>";
-    }
+    // if (count($notifications) > 0) {
+    //     foreach ($notifications as $notification) {
+    //         echo "<div class='notification'>" . $notification['message'] . " - " . $notification['created_at'] . "</div>";
+    //     }
+    // } else {
+    //     echo "<p>No notifications.</p>";
+    // }
     ?>
-</div>
+</div> -->
 <?php include 'footer.php'; ?>
