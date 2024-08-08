c33e9e3425148ab496e379498a6bd64fbfd14977
hotyute <hotyute_mason@hotmail.com>
2024-08-08 15:54:23 +0100

    modified:   header.php

--- header.php
+++ header.php
@@ -111,7 +111,7 @@ if (isset($_GET['logout'])) {
                     <li><a href='/admin/admin_panel.php'>Admin</a></li>
                 <?php endif; ?>
                 <?php if (isset($_SESSION['user_id']) && in_array($_SESSION['user_role'], ['admin', 'editor', 'member'])) : ?>
-                    <li><a href='/user_portal.php'>Admin</a></li>
+                    <li><a href='/user_portal.php'>User Portal</a></li>
                 <?php endif; ?>
                 <li><a href='<?php echo BASE_URL; ?>roster.php'>Roster</a></li>
                 <li><a href='<?php echo BASE_URL; ?>contact.php'>Contact Us</a></li>
