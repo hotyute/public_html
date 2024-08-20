12f70be08fe3ec39e217e075ec587a1a7f38ea12
hotyute <hotyute_mason@hotmail.com>
2024-08-20 05:13:28 +0100

    modified:   includes/sanitize.php

--- includes/sanitize.php
+++ includes/sanitize.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'session.php';
 require_once 'C:/htmlpurifier/library/HTMLPurifier.auto.php'; // Adjust the path based on where you extracted HTMLPurifier
+require_once '../htmlpurifier/HTMLPurifier_Filter_PageBreak.php';
 
 // CSRF token generation
 if (empty($_SESSION['csrf_token'])) {
@@ -13,12 +14,9 @@ function sanitize_html($content) {
     
     // Allow specific tags and attributes
     $config->set('HTML.Allowed', 'p,b,a[href],i,em,strong,ul,ol,li,br,span[style],div[style],img[src|alt|width|height]');
-
-    // Enable comments
-    $config->set('HTML.Comment', true);
-
-    // Add custom definition to allow <!-- pagebreak -->
-    $config->set('Core.CustomDocumentComments', array('pagebreak'));
+    
+    // Add the custom filter for pagebreak
+    $config->set('Filter.Custom', array(new HTMLPurifier_Filter_PageBreak()));
     
     $purifier = new HTMLPurifier($config);
     return $purifier->purify($content);
