025f7a67bdba645449f9e192738123964a9bdc92
hotyute <hotyute_mason@hotmail.com>
2024-08-22 13:02:32 +0100

    modified:   footer.php 	modified:   styles/header.css

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@ $footerLinks = [
 ];
 ?>
 
-<footer style="background-image: url('/images/banner.jpg'); background-repeat: no-repeat; background-size: cover; text-align: center; background-position: center -800px;">
+<footer style="background-image: url('/images/banner.jpg'); background-repeat: no-repeat; background-size: cover; text-align: center; background-position: center -600px;">
     <?php if (!isset($_SESSION['username'])) : ?>
         <p>Not registered yet? <a href="register.php">Register here</a></p>
     <?php endif; ?>
