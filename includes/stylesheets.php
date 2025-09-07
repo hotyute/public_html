2f8976f2eb109ba0a4fffccd809d6fadad1dfe14
hotyute <hotyute_mason@hotmail.com>
2025-09-07 17:02:51 +0100

    Refactor stylesheet inclusion to use a function for cache-busting and improve code readability; update index.css for better visual spacing and responsiveness

--- includes/stylesheets.php
+++ includes/stylesheets.php
@@ -1,51 +1,74 @@
-<?php $current_page = basename($_SERVER['SCRIPT_NAME'], '.php'); ?>
-<link rel="stylesheet" type="text/css" href="/styles/style.css">
-<link rel="stylesheet" type="text/css" href="/styles/header.css">
+<?php
+// includes/stylesheets.php
+$current_page = basename($_SERVER['SCRIPT_NAME'], '.php');
+
+function css($path) {
+    $full = $_SERVER['DOCUMENT_ROOT'] . $path;
+    $v = file_exists($full) ? filemtime($full) : time();
+    echo '<link rel="stylesheet" type="text/css" href="' . $path . '?v=' . $v . '">' . PHP_EOL;
+}
+?>
+<?php css('/styles/style.css'); ?>
+<?php css('/styles/header.css'); ?>
+
 <?php if ($current_page == 'index') : ?>
-    <link rel="stylesheet" type="text/css" href="/styles/surplus.css">
-    <link rel="stylesheet" type="text/css" href="/styles/featuredvid.css">
-    <link rel="stylesheet" type="text/css" href="/styles/sidebar.css">
-    <link rel="stylesheet" type="text/css" href="/styles/index.css">
+    <?php css('/styles/surplus.css'); ?>
+    <?php css('/styles/featuredvid.css'); ?>
+    <?php css('/styles/sidebar.css'); ?>
+    <?php css('/styles/index.css'); ?>  <!-- cache-busted -->
 <?php endif; ?>
+
 <?php if ($current_page == 'login') : ?>
-    <link rel="stylesheet" type="text/css" href="/styles/surplus.css">
+    <?php css('/styles/surplus.css'); ?>
 <?php endif; ?>
-<?php if (($current_page == 'admin_panel') || ($current_page ==  'edit_post') || ($current_page == 'create_post') || ($current_page == 'test_manage') || $current_page == 'user_portal') : ?>
-    <link rel="stylesheet" type="text/css" href="/styles/admin.css">
+
+<?php if (in_array($current_page, ['admin_panel','edit_post','create_post','test_manage','user_portal'])) : ?>
+    <?php css('/styles/admin.css'); ?>
     <?php if ($current_page == 'edit_post') : ?>
-        <link rel="stylesheet" type="text/css" href="/styles/edit_post.css">
+        <?php css('/styles/edit_post.css'); ?>
     <?php endif; ?>
 <?php endif; ?>
+
 <?php if ($current_page == 'post') : ?>
-    <link rel="stylesheet" type="text/css" href="/styles/post.css">
+    <?php css('/styles/post.css'); ?>
 <?php endif; ?>
+
 <?php if ($current_page == 'members') : ?>
-    <link rel="stylesheet" type="text/css" href="/styles/roster.css">
+    <?php css('/styles/roster.css'); ?>
 <?php endif; ?>
+
 <?php if ($current_page == 'contact') : ?>
-    <link rel="stylesheet" type="text/css" href="/styles/surplus.css">
-    <link rel="stylesheet" type="text/css" href="/styles/contact.css">
+    <?php css('/styles/surplus.css'); ?>
+    <?php css('/styles/contact.css'); ?>
 <?php endif; ?>
+
 <?php if ($current_page == 'archive') : ?>
-    <link rel="stylesheet" type="text/css" href="/styles/archive.css">
+    <?php css('/styles/archive.css'); ?>
 <?php endif; ?>
+
 <?php if ($current_page == 'manage_users') : ?>
-    <link rel="stylesheet" type="text/css" href="/styles/surplus.css">
-    <link rel="stylesheet" type="text/css" href="/styles/manage_users.css">
+    <?php css('/styles/surplus.css'); ?>
+    <?php css('/styles/manage_users.css'); ?>
 <?php endif; ?>
+
 <?php if ($current_page == 'notifications') : ?>
-    <link rel="stylesheet" type="text/css" href="/styles/notifications.css">
+    <?php css('/styles/notifications.css'); ?>
 <?php endif; ?>
+
 <?php if ($current_page == 'test_history') : ?>
-    <link rel="stylesheet" type="text/css" href="/styles/test_history.css">
+    <?php css('/styles/test_history.css'); ?>
 <?php endif; ?>
+
 <?php if ($current_page == 'register') : ?>
-    <link rel="stylesheet" type="text/css" href="/styles/register.css">
+    <?php css('/styles/register.css'); ?>
 <?php endif; ?>
+
 <?php if ($current_page == 'user_settings') : ?>
-    <link rel="stylesheet" type="text/css" href="/styles/user_settings.css">
+    <?php css('/styles/user_settings.css'); ?>
 <?php endif; ?>
+
 <?php if ($current_page == '404') : ?>
-    <link rel="stylesheet" type="text/css" href="/styles/404.css">
+    <?php css('/styles/404.css'); ?>
 <?php endif; ?>
-<link rel="stylesheet" type="text/css" href="/styles/footer.css">
+
+<?php css('/styles/footer.css'); ?>
